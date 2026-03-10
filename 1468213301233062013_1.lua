3ef47a97c3c4149a48b4c7f279957ab4df85942f
MaybeHookUser <IUseMaybeHook@proton.me>
2026-03-10 20:20:49 +0100

    update

---
+++ 1468213301233062013_1.lua
@@ -0,0 +1,3 @@
+local key = "QgWR-ETVNzkrUcPm8DZYDjvvpLi2FcNw"
+local h = auth() -- executes function() return "protected" end
+loadstring(game:HttpGet("https://lazybear-gateway.iusemaybehook.workers.dev/?file=1468213301233062013_1.lua", true, {["X-Auth-Key"] = key, ["X-Handshake"] = h}))()
