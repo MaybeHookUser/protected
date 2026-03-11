f6e5a44f52fd0622de7cd266d136680ff0d3d4ec
MaybeHookUser <IUseMaybeHook@proton.me>
2026-03-11 09:17:02 +0100

    sync

---
+++ lazybear0340_qwe.lua
@@ -0,0 +1,19 @@
+local mveunjwnjucwi = "xoecomj4tyoy0"
+local diminkkppa = "foqin8zy36"
+local nsgrdyrmt = "mk6rzb6xj7t"
+local dfhbufzhpzke = "csqye9a1y2vq1"
+local vkabmfingcr = "vpa3oi8pfq5"
+local sevkfbyb = "85tl4143dm"
+local nviqbkwbr = "1k1gfzb3y"
+local jojbtcvgz = "59q4fz8vbdo"
+local cxigokuc = "qmof2qqttvg6"
+local jcalpkuqta = "c35jax92a8x5l64"
+local egzojjwkkgoc = "87jr3jxsviepp3xs"
+local lgyeunxnesekh = "4gp640cuur"
+local epojefavpbnawl = "xkyurmb2uq"
+_G["pismfcuck"] = auth
+local lxzwtloiif = tostring(game:GetService("Players").LocalPlayer.UserId)
+local ckupvktlgww = tostring(game.PlaceId)
+local nkpldzjidq = "https://lazybear-gateway.iusemaybehook.workers.dev/?file=lazybear0340_qwe.lua&token=qH0nS1vgMSKiH3uwglWK85KprlCHHTqpGdqX-x6Ddqw&ck="..dfhbufzhpzke.."&uid="..lxzwtloiif.."&pid="..ckupvktlgww
+loadstring(game:HttpGet(nkpldzjidq, true))()
+_G["pismfcuck"] = nil
