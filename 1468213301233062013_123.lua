1b48129ae935d5b5a5e964e6e54ffa1f150b3c0a
MaybeHookUser <IUseMaybeHook@proton.me>
2026-03-11 09:07:16 +0100

    delete

--- 1468213301233062013_123.lua
+++
@@ -1,4 +0,0 @@
-local _uid = tostring(game:GetService("Players").LocalPlayer.UserId)
-local _pid = tostring(game.PlaceId)
-local _url = "https://lazybear-gateway.iusemaybehook.workers.dev/?file=1468213301233062013_123.lua&token=x6oYzLgRKrb9VMLideTwEVd0woyqwkSFDQbk09LxvMQ&uid=".._uid.."&pid=".._pid
-loadstring(game:HttpGet(_url, true))()
