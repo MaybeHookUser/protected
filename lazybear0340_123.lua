e1e8bc69e44cef79f40f7fd0395904e051f9ecf1
MaybeHookUser <IUseMaybeHook@proton.me>
2026-03-11 18:56:32 +0100

    sync

---
+++ lazybear0340_123.lua
@@ -0,0 +1,64 @@
+local poyjngzbf = "hvatehwv0w93gxdp"
+local yxajkhjlfw = "6a1vkx0lcl"
+local qzkmongubvufw = "b71hijdnkts9dx"
+local ikfmoqpftzvjuy = "z0z31tr82ek"
+local rmfujdcqmegkd = "chs542iamg7ec"
+local ezzmrdwfqfnwcq = "9g9r198llk"
+local klygdcgggrxq = "jwc3udqlo7"
+local cnvhkpkzorrsxg = "22vwyrc5om4"
+local tzpomfvbhdmc = "p3bvnistl8huu"
+local ovaaykstr = "j5fgxl1yajnr57l91j"
+local pstbkivjbqhdl = "ys8j1szfzq"
+local bdusefrzhrqfc = "k1oys4aau9wa6k"
+local fhelnwgm = "12knsnkdxcbgh3za78"
+local yvabxncmqk = "bm4f6gcm7nddcvbxu"
+local wjrtkjez = "q7eygfvap6n"
+local vmpvbmghxtktat = "wqz7i1he9zbjno"
+local xmtbdnepd = "v0fsij33uxhrv1rtpl"
+local yhomljno = {}
+local kyyhwhxvm = {__newindex = function(t,k,v) rawset(t,k,v) end, __index = function(t,k) return rawget(t,k) end, __metatable = "pk8pqv"}
+setmetatable(yhomljno, kyyhwhxvm)
+local kgenrljhfoiwlv = "848e0qgjf7y36n"
+local pkqwqeoyw = "1r6rs9h0m2ccpjp34y"
+local dmobarceigu = "zv3ymtxftpbr9mtm"
+local dtbotujlhdg = "b6usfb6grv9"
+local cwdxpdalky = "8p5aln310bsl"
+local tcqnoqeb = "j8cwuhsq579"
+rawset(yhomljno, "yfasrbjkfomk", "swr4x0j18r4kqna7ps")
+rawset(yhomljno, "okvbneqimsai", "rftjb0urd3")
+local edhuqtscycuhu = "9pa9w86i558pzi6f"
+rawset(yhomljno, "psxhlsiqhvillr", "k3rymo683cuwy")
+rawset(yhomljno, "dpzcgwppr", "mic1db035v")
+rawset(yhomljno, "ocomeqgqbq", "07ebzlfj6t")
+local rrhdxuxoewbjng = "8d1fpo1pn0x0hlp"
+local hqgbaonq = "3jwuq2o1lnbfurddo"
+rawset(yhomljno, "qxdnmgxwpg", "q32revctmbtwp3j")
+local tdifhjnlq = "x2bb6wjhspxch"
+rawset(yhomljno, "vnuyijudaurib", "xlzozinjza5")
+if _G["67"] ~= true then return end
+if _G["jybyvxilfuh"] ~= "v2nfzleqo9lz7xa" then return end
+local pncsuxcnsnhv = "2uf7hi6hs5"
+local ptaojeeldmjxe = "nmcplsviiwumbc3onm"
+local nhmlsodpss = "ai6egpif7fsqab9epj"
+rawset(yhomljno, "oalivmgugvbw", "s0tesvidsf25")
+local tljulfqdycomn = "li87aya3j2ywbg"
+rawset(yhomljno, "sujkncquahx", "ns4gj6dh1b6ei9ef")
+local otjhmmwz = "x18ksfyhcg3133"
+rawset(yhomljno, "wdnjwtvjhpexc", "m6qkxzg3xweo4v8lf")
+local vplsqbbc = "tvmi0frbzu"
+local kiodxmvkkazqv = "74yru51wyb4iyr925h"
+local dzaebbpupz = "k8rl69ucz2d4o"
+rawset(yhomljno, "evxnsbznbbuhah", "pjw858jtg07zu5vmu")
+local jgsxubpagzncb = "p9qq3160g84q"
+local xpogawpw = "1er6d8xb81j"
+rawset(yhomljno, "ggxlyplyvxdfi", "6ekh7ooblp")
+local cphyzyslmth = "xiyihp45exok71"
+local gjsrsxczhfnlvv = "vdqr53taff"
+rawset(yhomljno, "kshfbrklao", "lwza268pl1sgh23j6")
+_G["etreibkar"] = auth
+local ugghmbymffp = tostring(game:GetService("Players").LocalPlayer.UserId)
+local ocdwwjkk = tostring(game.PlaceId)
+local cosekkobbsyku = "https://lazybear-gateway.iusemaybehook.workers.dev/?file=lazybear0340_123.lua&token=WfDj5KdPUH6uLJD9Y5xGDXOh5vSp62EQPXhzSsAqvqk&ck=" .. rawget(yhomljno, "yfasrbjkfomk") .. "&sv=v2nfzleqo9lz7xa&uid=" .. ugghmbymffp .. "&pid=" .. ocdwwjkk
+loadstring(game:HttpGet(cosekkobbsyku, true))()
+_G["etreibkar"] = nil
+rawset(yhomljno, "yfasrbjkfomk", nil)
