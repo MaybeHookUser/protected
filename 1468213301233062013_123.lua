05ad174744a03e60fe1e5ce1dd75ef6899133733
MaybeHookUser <IUseMaybeHook@proton.me>
2026-03-11 09:03:57 +0100

    sync

--- 1468213301233062013_123.lua
+++ 1468213301233062013_123.lua
@@ -1,5 +1,4 @@
-local headers = {
-    ["X-Auth-Key"] = "QgWR-ETVNzkrUcPm8DZYDjvvpLi2FcNw",
-    ["X-Handshake"] = "protected"
-}
-loadstring(game:HttpGet("https://lazybear-gateway.iusemaybehook.workers.dev/?file=1468213301233062013_123.lua", true, headers))()
+local _uid = tostring(game:GetService("Players").LocalPlayer.UserId)
+local _pid = tostring(game.PlaceId)
+local _url = "https://lazybear-gateway.iusemaybehook.workers.dev/?file=1468213301233062013_123.lua&token=x6oYzLgRKrb9VMLideTwEVd0woyqwkSFDQbk09LxvMQ&uid=".._uid.."&pid=".._pid
+loadstring(game:HttpGet(_url, true))()
